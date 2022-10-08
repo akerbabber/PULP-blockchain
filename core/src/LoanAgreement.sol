409810290f26a8a2a5335ae5bc4ffadbdd2ac394
Naama Paulemont <60440166+junaama@users.noreply.github.com>
2022-10-08 17:06:07 -0400

    add skeleton functions to contracts

--- core/src/LoanAgreement.sol
+++ core/src/LoanAgreement.sol
@@ -1,17 +1,28 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
+import "./PulpLibrary.sol";
 
 contract LoanAgreement {
-    struct Lender {
-        address lenderAddress;
-        uint256 ageThreshold;
-        uint256 reputationThreshold;
+    using PulpLibrary for PulpLibrary.Lender;
+    using PulpLibrary for PulpLibrary.Borrower;
+    using PulpLibrary for PulpLibrary.Agreement;
+
+    mapping(address => PulpLibrary.Lender) internal lenderData;
+    mapping(address => PulpLibrary.Borrower) internal borrowerData;
+    mapping(address => PulpLibrary.Agreement) internal loanAgreements;
+
+    function createLoanAgreement(PulpLibrary.Lender memory lender, PulpLibrary.Borrower memory borrower) public {
+        // create loan agreement
+    }
+    function removeLoanAgreement() public {
+        // remove loan agreement
     }
 
-    struct Borrower {
-        address borrowerAddress;
-        bool ageThresholdMet;
-        bool reputationThresholdMet;
+    function updateLoanAgreement() public {
+        // update certain loan agreement attributes
+    }
+    function getLoanAgreement() public returns (PulpLibrary.Agreement memory){
+        // get loan agreement
     }
 
 }
--- core/src/LoanOffer.sol
+++ core/src/LoanOffer.sol
@@ -1,4 +1,25 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract LoanOffer {}
+contract LoanOffer {
+    struct Offer {
+        uint256 ageThreshold;
+        uint256 reputationThreshold;
+        uint256 loanOfferAmount;
+    }
+
+    Offer[] public loanOffers;
+
+    function addLoanOffer() internal returns (Offer memory) {
+        // add all loan offers
+    }
+    
+    function removeLoanOffer() external {
+        // done automatically by met requirements
+        // triggered by loan user
+    }
+
+    function getLoanOffers() external view returns (Offer[] memory){
+        // get all loan offers
+    }
+}
