79d646b5b02a441f8f856ac2fd59b3e40e1dd744
Naama Paulemont <60440166+junaama@users.noreply.github.com>
2022-10-08 17:00:23 -0400

    add price oracle and core contract

--- core/src/PulpCore.sol
+++ core/src/PulpCore.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.13;
 
 contract PulpCore {
-
+    // change to use oracle for price settlement
+    uint256 public constant MAX_LOAN_AMOUNT = 10000;
+    uint256 public constant DEFAULT_INTEREST_RATE = 15;
 
 }
